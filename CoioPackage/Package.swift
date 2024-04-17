c6d9500d1099b52115b035e4004b8620af55ea57
Kanta Oikawa <k.oikawa@kantacky.com>
2024-04-17 10:13:21 +0900

    Add CoioButton

--- CoioPackage/Package.swift
+++ CoioPackage/Package.swift
@@ -7,9 +7,14 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v17)],
     products: [
+        .library(name: "CoioComponent", targets: ["CoioComponent"]),
         .library(name: "CoioPackage", targets: ["CoioPackage"]),
     ],
     targets: [
+        .target(
+            name: "CoioComponent",
+            dependencies: ["CoioResource"]
+        ),
         .target(name: "CoioEntity"),
         .target(name: "CoioPackage"),
         .testTarget(
