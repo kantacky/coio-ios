d6641360d009c914f5a44701e96ccb1b1806ed97
Kanta Oikawa <k.oikawa@kantacky.com>
2024-04-17 09:25:09 +0900

    Add font style

---
+++ CoioPackage/Sources/CoioResource/Font+.swift
@@ -0,0 +1,15 @@
+//
+//  Font+.swift
+//
+//
+//  Created by Kanta Oikawa on 2024/04/17.
+//
+
+import SwiftUI
+
+public extension Font {
+    static let coioTitle = Font.system(size: 32, weight: .bold)
+    static let coioSubtitle = Font.system(size: 20, weight: .bold)
+    static let coioBody = Font.system(size: 12, weight: .regular)
+    static let coioCaption = Font.system(size: 8, weight: .regular)
+}
